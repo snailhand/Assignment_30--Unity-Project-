3d6bc47f908b70f00f8fa1bb161781cd7fd9847c
simkaiwen <kaiphonegt@gmail.com>
2020-10-21 20:52:18 +0800

    2nd Lesson
    -double jumps
    -new boundary
    -projectile cube

---
+++ Assignment_30/Assets/Scripts/MovePositiveZ.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositiveZ : MonoBehaviour
+{
+    float forwardSpeed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.z > 22)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+        }
+        
+    }
+}
--- Assignment_30/Assets/Scripts/PlayerController.cs
+++ Assignment_30/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 public class PlayerController : MonoBehaviour
 {
     float speed = 10.0f;
     float jumpheight = 15.0f;
-    float zLimit = 4.8f;
-    float xLimit = 4.8f;
+    float djumpheight = 15.0f;
+    float zLimit = 6.5f;
+    float xLimit = 6.5f;
     float gravityMod = 3.0f;
+    bool isOnGround = true;
+    float jumptimes = 0f;
+    float jumpcount = 0f;
 
     Rigidbody playerRb;   //referencing 
 
@@ -33,25 +39,22 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
-        if (transform.position.y <= 0.5)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                playerRb.AddForce(Vector3.up * jumpheight, ForceMode.Impulse);
-            }
-        }
+
+        PlayerJump();
+        
 
         //Setting Boundaries within the plane
-
+        /*
         //front and back
         if (transform.position.z < -zLimit)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, -zLimit);
         }
         else if (transform.position.z > zLimit)
-        {
+        { 
             transform.position = new Vector3(transform.position.x, transform.position.y, zLimit);
         }
+
 
         //left and right
         if (transform.position.x < -xLimit)
@@ -62,14 +65,47 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
         }
+        */
+    }
 
-
+    //Method Call from UnityEngine (Triggers when Object collides with ground)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            jumpcount = 0;
+        }
 
     }
 
-    void MovePlayer()
+    //Player Jumping Method
+    void PlayerJump()
     {
+        if (isOnGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && jumpcount == 0)
+            {
+                //Debug.Log("Player JUMPING Y Position: " + transform.position.y);
+                playerRb.AddForce(Vector3.up * jumpheight, ForceMode.Impulse);
+                isOnGround = false;
+                jumptimes += 1;
+                Debug.Log("Successful Jumps: " + jumptimes);
 
+                //doublejump
+                jumpcount += 1;
+            }
 
+        }
+        
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && jumpcount == 1)
+            {
+                playerRb.AddForce(Vector3.up * djumpheight, ForceMode.Impulse);
+                jumpcount += 1;
+            }
+        }
+        
     }
 }
