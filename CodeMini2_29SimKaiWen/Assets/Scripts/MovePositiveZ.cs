3c59805f861800f510cc9d81158b795fd4586c22
simkaiwen <kaiphonegt@gmail.com>
2020-11-04 16:17:27 +0800

    moving Platform

---
+++ CodeMini2_29SimKaiWen/Assets/Scripts/MovePositiveZ.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePositiveZ : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    public float cubezLimit = 26.0f;
+    float startPos;
+    public bool forwardMove = true;
+    bool ridingCube = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovingCube();
+
+    }
+
+    public void MovingCube()
+    {
+        //moving cube
+
+        if (transform.position.z < cubezLimit && forwardMove)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        if (!forwardMove)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+
+        }
+
+        //checking direction 
+
+        if (transform.position.z <= startPos)
+        {
+            forwardMove = true;
+        }
+        if (transform.position.z >= cubezLimit)
+        {
+            forwardMove = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ridingCube = true;
+            collision.collider.transform.SetParent(transform);
+
+        }
+       
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            ridingCube = false;
+            collision.collider.transform.SetParent(null);
+
+        }
+
+    }
+
+}
---
+++ CodeMini2_29SimKaiWen/Assets/Scripts/PlayerController29.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController29 : MonoBehaviour
+{
+    //Global Variables:
+
+    float speed = 14.0f;
+    float jumpheight = 15.0f;
+    float zLimit = 30f;
+    float xLimit = 4.5f;
+    float gravityMod = 3.0f;
+    bool isOnGround = true;
+    bool ridingCube = false;
+    float jumpCount = 0;
+
+    //referencing 
+    Rigidbody playerRb;   
+    public GameObject MoveCube;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody link
+
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Declare and Initialize variables to reference to User Interaction Inputs
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //Keybind Controls (GameObject) 
+        transform.Translate(Vector3.forward * Time.deltaTime * horizontalInput * speed);
+        transform.Translate(Vector3.left * Time.deltaTime * verticalInput * speed);
+
+        PlayerJump();
+      
+
+        //Setting Boundaries within the plane
+
+        //front and back
+        if (transform.position.z < -9)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -9);
+        }
+        else if (transform.position.z > zLimit)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zLimit);
+        }
+
+
+        //left and right
+        if (transform.position.x < -xLimit)
+        {
+            transform.position = new Vector3(-xLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > xLimit)
+        {
+            transform.position = new Vector3(xLimit, transform.position.y, transform.position.z);
+        }
+
+
+    }
+
+    //Method Call from UnityEngine (Triggers when Object collides with ground)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            jumpCount = 0;
+        }
+        if (collision.gameObject.CompareTag("MoveCube"))
+        {
+            ridingCube = true;
+            jumpCount = 0;
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (isOnGround || ridingCube && jumpCount == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                //Debug.Log("Player JUMPING Y Position: " + transform.position.y);
+                playerRb.AddForce(Vector3.up * jumpheight, ForceMode.Impulse);
+                isOnGround = false;
+                jumpCount += 1;
+
+            }
+
+        }
+    }
+    
+}
