932ee0c0b8a227f0331eb554a844b3aab47ef4f8
simkaiwen <kaiphonegt@gmail.com>
2020-11-05 17:26:37 +0800

    Ui Score

---
+++ 29_SimKaiWen_Lab08/Assets/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScript : MonoBehaviour
+{
+    public GameObject scoreText;
+    float spaceCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "Start Function";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spaceCount++;
+            scoreText.GetComponent<Text>().text = ("SpaceBar: "+spaceCount);
+        }
+    }
+}
