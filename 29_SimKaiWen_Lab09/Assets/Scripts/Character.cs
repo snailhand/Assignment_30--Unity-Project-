798879e881bb90a1d4d916ec2436ee63b0e52d4c
simkaiwen <kaiphonegt@gmail.com>
2020-11-09 21:58:48 +0800

    yes

---
+++ 29_SimKaiWen_Lab09/Assets/Scripts/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //forward and backwards 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * speed);
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //transform.position += new Vector3(0, 0, Time.deltaTime * -speed);
+            transform.position += transform.forward * -speed * Time.deltaTime;
+        }
+
+        //rotating object left and right
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * -rotateSpeed, 0));
+        }
+
+        text.GetComponent<Text>().text = ("Position: (" + ("{0:00.0}",transform.position.x) + "," + transform.position.y + "," + transform.position.z + ")") ;
+
+    }
+}
